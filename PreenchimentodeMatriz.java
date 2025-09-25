066e6dbb2f2afd99a896ff2c90714a749a24dfbc
joaobezerra0 <cjoaobezerrapp@gmail.com>
2025-09-25 01:08:40 -0300

    Atividade - Preenchimento de Matriz
    Este código cria uma matriz 3x3, lê 9 números do usuário e exibe a matriz formatada linha por linha.

---
+++ PreenchimentodeMatriz.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PreenchimentodeMatriz {
+    public static void main(String[] args) {
+        int[][] matriz = new int[3][3];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Digite o elemento [" + i + "][" + j + "]: ");
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Matriz:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
